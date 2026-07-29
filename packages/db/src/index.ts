18d47a135234a9ceaacb86667a8d15b33fddd63a
vokerg <vokerg@gmail.com>
2026-07-29 13:27:02 +0200

    feat: expose score breakdown schemas through db package

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,7 @@
 export { sql } from 'kysely';
 export type { Kysely } from 'kysely';
+export { DailyScoreBreakdownSchema, IsoDateSchema } from '@sportos/shared';
+export type { DailyScoreBreakdown } from '@sportos/shared';
 export * from './schema.js';
 export * from './repository-contracts.js';
 export * from './pool.js';
