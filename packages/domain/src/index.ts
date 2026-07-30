a74983b7e0ebc00b5f0af35355d6cebabf2102e4
vokerg <vokerg@gmail.com>
2026-07-30 07:05:13 +0200

    feat(domain): export reconciliation contracts

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -1,5 +1,6 @@
 export * from './types.js';
 export * from './units.js';
 export * from './scoring.js';
+export * from './reconciliation.js';
 export * from './performance.js';
 export * from './daily.js';
