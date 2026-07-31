952b982b0592ebc8d938ceda773edf130c1b4b9e
vokerg <vokerg@gmail.com>
2026-07-31 11:02:12 +0200

    feat(domain): export Rules Studio contract

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -2,5 +2,6 @@ export * from './types.js';
 export * from './units.js';
 export * from './scoring.js';
 export * from './reconciliation.js';
+export * from './rules-studio.js';
 export * from './performance.js';
 export * from './daily.js';
