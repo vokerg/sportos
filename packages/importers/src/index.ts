f146d8547ddb9781281771392eb13b156f88df36
vokerg <vokerg@gmail.com>
2026-07-31 08:03:36 +0200

    test: export deterministic workbook fixture writer

--- packages/importers/src/index.ts
+++ packages/importers/src/index.ts
@@ -3,3 +3,4 @@ export * from './my-sport.importer.js';
 export * from './run-db.importer.js';
 export * from './import-service.js';
 export * from './upload-storage.js';
+export { writeMySportFixture } from './test-fixtures/xlsx-fixtures.js';
