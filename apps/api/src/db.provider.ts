63205f64f2b068fb8ef568c19f842b0cae1eb463
vokerg <vokerg@gmail.com>
2026-08-01 19:52:03 +0200

    feat(api): add account-scoped database execution

--- apps/api/src/db.provider.ts
+++ apps/api/src/db.provider.ts
@@ -1,9 +1,13 @@
 import { Injectable, OnModuleDestroy } from '@nestjs/common';
-import { createDb, type Database, type Kysely } from '@sportos/db';
+import { createDb, withAccountContext, type Database, type Kysely } from '@sportos/db';
 
 @Injectable()
 export class DbProvider implements OnModuleDestroy {
   readonly db: Kysely<Database> = createDb();
+
+  withAccount<T>(accountId: string, callback: (db: Kysely<Database>) => Promise<T>): Promise<T> {
+    return withAccountContext(this.db, accountId, callback);
+  }
 
   async onModuleDestroy() {
     await this.db.destroy();
