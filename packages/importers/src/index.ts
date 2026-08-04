0224c8d39b4446f2f62f389e92afc05f19f55a78
vokerg <vokerg@gmail.com>
2026-08-04 19:58:34 +0200

    refactor(providers): export provider ingestion contracts

--- packages/importers/src/index.ts
+++ packages/importers/src/index.ts
@@ -3,4 +3,7 @@ export * from './my-sport.importer.js';
 export * from './run-db.importer.js';
 export * from './import-service.js';
 export * from './upload-storage.js';
+export * from './provider-types.js';
+export * from './credential-cipher.js';
+export * from './strava-adapter.js';
 export { writeMySportFixture } from './test-fixtures/xlsx-fixtures.js';
