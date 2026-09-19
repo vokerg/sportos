e129b3d7460a68515cc58a84c32e81100b6f642e
vokerg <vokerg@gmail.com>
2026-09-19 15:58:06 +0200

    Centralize Angular bootstrap configuration

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,25 +1,5 @@
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
-import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
-import { provideEchartsCore } from 'ngx-echarts';
-import * as echarts from 'echarts/core';
-import { BarChart, LineChart } from 'echarts/charts';
-import { GridComponent, LegendComponent, TooltipComponent } from 'echarts/components';
-import { CanvasRenderer } from 'echarts/renderers';
 import { AppComponent } from './app/app.component';
-import { APP_ROUTES } from './app/app.routes';
-import { authHttpInterceptor } from './app/auth-http.interceptor';
+import { appConfig } from './app/app.config';
 
-ModuleRegistry.registerModules([AllCommunityModule]);
-echarts.use([BarChart, LineChart, GridComponent, LegendComponent, TooltipComponent, CanvasRenderer]);
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(withInterceptors([authHttpInterceptor])),
-    provideAnimations(),
-    provideRouter(APP_ROUTES),
-    provideEchartsCore({ echarts }),
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
